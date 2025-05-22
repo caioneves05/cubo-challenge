5fbadfa1ba56ab6b8a98adce5dba65caebd57dae
Caio Neves <ncaio037@gmail.com>
2025-05-21 22:09:05 -0300

    initial

---
+++ Cubo-Challenge/src/main/java/com/example/Cubo/Challenge/CuboChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cubo.Challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuboChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuboChallengeApplication.class, args);
+	}
+
+}
---
+++ Cubo-Challenge/src/test/java/com/example/Cubo/Challenge/CuboChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cubo.Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuboChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
